2a5602066bdfc42a00e7800db7aebbed492d800d
jatuponnop <jatuponnop@gmail.com>
2022-11-15 18:03:55 +0700

    add filter

---
+++ src/components/filter/Filter.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import styles from "./Filter.module.scss";
+
+const Filter = () => {
+  return (
+    <div className={styles.filter}>
+      <label className={`${styles.title} mb-1`}>ค้นหา </label>
+      <select>
+        <option defaultChecked="true">ปี ค.ศ.</option>
+        <option>ค.ศ. 1902</option>
+      </select>
+      <select>
+        <option>หมวดหมู่</option>
+        <option>เคมี</option>
+        <option>วิทยาศาสตร์เศรษฐกิจ</option>
+        <option>วรรณกรรม</option>
+        <option>สันติภาพ</option>
+        <option>ฟิสิกส์</option>
+        <option>สรีรวิทยา/การแพทย์</option>
+      </select>
+      <button className='btn'>ค้นหา</button>
+      <div className={styles.prizeAmount}>
+        <label className={styles.prizeAmountText}>
+          จำนวนรางวัล
+        </label>
+        <label className={styles.prizeAmountValue}>
+          150,782
+        </label>
+      </div>
+      <img src="nobel-prize.png" className={styles.img} />
+    </div>
+  )
+}
+
+export default Filter
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Header from '../components/header/Header'
+import Filter from '../components/filter/Filter'
 
 const Home = () => {
   return (
     <>
       <Header />
+      <div className="container">
+        <Filter />
+      </div>
     </>
   )
 }
