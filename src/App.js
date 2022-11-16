0c967ff228428f3b18e5cf01ef699a23b6b89fc4
jatuponnop <jatuponnop@gmail.com>
2022-11-16 13:39:09 +0700

    deploy project

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import "./styles/global.scss"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
+    <Home />
   );
 }
 
